5ef8241fc1d47e38fc7e01d18516b22ff102168d
黑黑黑 <2270716426@qq.com>
2021-11-08 08:55:28 +0800

    11111

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router/index.js'
 //引入ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
+//ElementUI改中文
+import locale from "element-plus/lib/locale/lang/zh-cn"
 //导库
 import VueAxios from 'vue-axios'
 import axios from 'axios'
@@ -13,4 +15,4 @@ axios.defaults.baseURL = 'http://localhost:8848/';
 //设置默认的请求超时时间。例如超过了5s，就会告知用户当前请求超时，请刷新等。
 axios.defaults.timeout = 5000;
 
-createApp(App).use(router).use(VueAxios,axios).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(VueAxios,axios).use(ElementPlus,{locale}).mount('#app')
